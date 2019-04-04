4780bfac3d6c5b91a1d4d910263cb2015c7ff846
Andrey Shilov <andreyshilov1985@bk.ru>
2019-04-04 04:40:28 +0300

    Added abstraction of the first component Board

---
+++ src/components.js
@@ -0,0 +1,5 @@
+import Board from './components/Board/Board';
+
+export {
+	Board,
+}
---
+++ src/components/Board/Board.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './Board.scss';
+
+class Board extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="board">
+				Board
+			</div>
+		)
+	}
+}
+
+export default Board;
